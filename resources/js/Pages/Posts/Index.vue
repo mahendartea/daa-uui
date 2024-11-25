bc6c90fa66219dbbeee2e316bbc665328ac597ed
Mahendar Dwi Payana <38557290+mahendartea@users.noreply.github.com>
2024-11-25 21:13:30 +0700

    fix : posts

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -1,46 +1,52 @@
 <template>
+
+    <Head>
+        <title>Posts</title>
+        <meta name="description" content="Posts" />
+    </Head>
     <LoggedInLayout title="Posts">
-        <Toast class="z-50"/>
+        <Toast class="z-50" />
         <div class="card flex justify-between my-5">
-            <Breadcrumb :home="home" :model="items" class="bg-gray-50 dark:bg-gray-800 w-full rounded-sm"/>
+            <Breadcrumb :home="home" :model="items" class="bg-gray-50 dark:bg-gray-800 w-full rounded-sm" />
         </div>
         <div>
             <div class="flex justify-end gap-3 mb-2">
                 <FloatLabel variant="on">
-                    <InputText id="on_label" v-model="search" class="w-72"/>
+                    <InputText id="on_label" v-model="search" class="w-72" />
                     <label for="on_label">Cari Postingan...</label>
                 </FloatLabel>
-                <Button class="p-button-primary p-button-outlined hover:bg-primary-500 hover:text-white" icon="pi pi-plus" icon-class="p-w-4" label="Tambah Postingan"
-                        type="button"
-                        @click="createPost"/>
+                <Button class="p-button-primary p-button-outlined hover:bg-primary-500 hover:text-white"
+                    icon="pi pi-plus" icon-class="p-w-4" label="Tambah Postingan" type="button" @click="createPost" />
             </div>
             <Card class="border">
                 <template #title>Postingan</template>
                 <template #content>
-                    <DataTable :value="formattedPosts" responsiveLayout="scroll" size="small"
-                               stripedRows tableStyle="min-width: 50rem">
-                        <Column field="id" header="ID" sortable style="width: 10%;"/>
-                        <Column field="title" header="Judul" sortable style="width: 40%;"/>
-                        <Column field="category" header="Kategori" sortable style="width:10%;"/>
-                        <Column :body="formatDate" field="updated" header="Diubah" sortable style="width: 15%;"/>
-                        <Column field="tag" header="Tag"/>
+                    <DataTable :value="formattedPosts" responsiveLayout="scroll" size="small" stripedRows
+                        tableStyle="min-width: 50rem">
+                        <Column field="id" header="ID" sortable style="width: 10%;" />
+                        <Column field="title" header="Judul" sortable style="width: 40%;" />
+                        <Column field="category" header="Kategori" sortable style="width:10%;" />
+                        <Column :body="formatDate" field="updated" header="Diubah" sortable style="width: 15%;" />
+                        <Column field="tag" header="Tag" />
                         <Column field="actions" header="Aksi" style="width: 13%;">
                             <template #body="{ data }">
                                 <div class="grid grid-cols-2 gap-1 gap-y-2">
-                                    <Button class="p-button-secondary p-button-outlined hover:bg-primary-500 hover:text-white" icon="pi pi-pencil" icon-class="p-w-4" label="Ubah"
-                                            size="small"
-                                            type="button" @click="editPost(data.id)"/>
-                                    <Button class="p-button-danger p-button-outlined hover:bg-primary-500 hover:text-white" icon="pi pi-trash" icon-class="p-w-4"
-                                            label="Hapus"
-                                            size="small"
-                                            @click="confirmDelete(data.id)"/>
-                                    <Button class="p-button-info p-button-outlined hover:bg-primary-500 hover:text-white" icon="pi pi-eye" icon-class="p-w-4" label="view"
-                                            size="small"
-                                            type="button" @click="viewPost(data)"/>
-                                    <Button v-if="data.draft == 1" class="p-button-success p-button-outlined hover:bg-primary-500 hover:text-white" icon="pi pi-save" icon-class="p-w-4"
-                                            label="Draf" size="small"
-                                            type="button"
-                                            @click="publishPost(data.id)"/>
+                                    <Button
+                                        class="p-button-secondary p-button-outlined hover:bg-primary-500 hover:text-white"
+                                        icon="pi pi-pencil" icon-class="p-w-4" label="Ubah" size="small" type="button"
+                                        @click="editPost(data.id)" />
+                                    <Button
+                                        class="p-button-danger p-button-outlined hover:bg-primary-500 hover:text-white"
+                                        icon="pi pi-trash" icon-class="p-w-4" label="Hapus" size="small"
+                                        @click="confirmDelete(data.id)" />
+                                    <Button
+                                        class="p-button-info p-button-outlined hover:bg-primary-500 hover:text-white"
+                                        icon="pi pi-eye" icon-class="p-w-4" label="view" size="small" type="button"
+                                        @click="viewPost(data)" />
+                                    <Button v-if="data.draft == 1"
+                                        class="p-button-success p-button-outlined hover:bg-primary-500 hover:text-white"
+                                        icon="pi pi-save" icon-class="p-w-4" label="Draf" size="small" type="button"
+                                        @click="publishPost(data.id)" />
                                 </div>
                             </template>
                         </Column>
@@ -58,9 +64,8 @@
 
                 <template #footer>
                     <Pagination :currentPage="posts.current_page" :from="posts.from" :lastPage="posts.last_page"
-                                :links="posts.links" :nextPageUrl="posts.next_page_url"
-                                :prevPageUrl="posts.prev_page_url"
-                                :to="posts.to" :total="posts.total"/>
+                        :links="posts.links" :nextPageUrl="posts.next_page_url" :prevPageUrl="posts.prev_page_url"
+                        :to="posts.to" :total="posts.total" />
                 </template>
             </Card>
         </div>
@@ -69,18 +74,19 @@
 
 <script setup>
 import LoggedInLayout from '@/Layouts/LoggedInLayout.vue';
-import {Card, Toast} from 'primevue';
+import { Card, Toast } from 'primevue';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import Pagination from '@/Components/Pagination.vue';
-import {computed, onMounted, ref, watch} from 'vue';
+import { computed, onMounted, ref, watch } from 'vue';
 import Breadcrumb from 'primevue/breadcrumb';
 import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
 import FloatLabel from 'primevue/floatlabel';
-import {router} from '@inertiajs/vue3';
+import { router } from '@inertiajs/vue3';
 import Dialog from 'primevue/dialog';
-import {useToast} from 'primevue/usetoast';
+import { useToast } from 'primevue/usetoast';
+import { Head } from '@inertiajs/vue3';
 
 const toast = useToast();
 const props = defineProps({
@@ -117,16 +123,16 @@ const home = ref({
     icon: 'pi pi-home'
 });
 const items = ref([
-    {label: 'Postingan'},
-    {label: 'Data'},
+    { label: 'Postingan' },
+    { label: 'Data' },
 ]);
 
 const search = ref('');
 
 watch(search, (mencari) => {
     router.get('/admin/posts', {
-            search: mencari
-        }, {preserveState: true}
+        search: mencari
+    }, { preserveState: true }
     );
 });
 
--- resources/js/Pages/Posts/Show.vue
+++ resources/js/Pages/Posts/Show.vue
@@ -1,10 +1,10 @@
 <script setup>
 import LoggedInLayout from '@/Layouts/LoggedInLayout.vue';
 import Breadcrumb from 'primevue/breadcrumb';
-import {ref} from 'vue';
+import { ref } from 'vue';
 import Card from "primevue/card";
-import {Button} from "primevue";
-import {router} from "@inertiajs/vue3";
+import { Button } from "primevue";
+import { router } from "@inertiajs/vue3";
 
 defineProps({
     post: Object
@@ -13,8 +13,8 @@ const home = ref({
     icon: 'pi pi-home'
 });
 const items = ref([
-    {label: 'Postingan'},
-    {label: 'Detail'},
+    { label: 'Postingan' },
+    { label: 'Detail' },
 ]);
 
 const formatDate = (dateString) => {
@@ -29,44 +29,42 @@ const formatDate = (dateString) => {
 </script>
 
 <template>
-    <LoggedInLayout>
+    <LoggedInLayout title="Detail Postingan">
         <div class="card flex justify-between my-5">
-            <Breadcrumb :home="home" :model="items" class="bg-gray-50 dark:bg-gray-800 w-full rounded-sm"/>
+            <Breadcrumb :home="home" :model="items" class="bg-gray-50 dark:bg-gray-800 w-full rounded-sm" />
         </div>
-        <Card>
+        <Card class="border bg-gray-50">
             <template #title>
-                <h2 class="text-2xl font-extrabold text-primary-emphasis-alt">{{ post.title }}
+                <h2 class="text-2xl font-bold text-primary p-3">{{ post.title }}
                     <sup v-if="post.draft == true" class="text-white bg-red-400 px-1 rounded">Draf</sup>
                 </h2>
             </template>
             <template #content>
-                <div class="mt-3 mb-5 bg-highlight text-white font-bold text-lg flex justify-between px-3">
+                <div class="mt-3 mb-5 bg-primary text-white font-bold text-lg flex justify-between px-3">
                     <span>{{ post.category }}</span>
-                    <span>Di Update pada : {{ formatDate(post.updated) }}</span>
                 </div>
                 <div class="mb-10">
                     <p class="font-bold inline-block">Tag :</p>
                     <span v-for="tag in post.tag.split(',')" :key="tag.trim()"
-                          class="tag-item mx-0.5 bg-highlight px-1 rounded">{{
+                        class="tag-item mx-0.5 bg-gray-200 px-1 rounded">{{
                             tag.trim()
                         }}</span>
                 </div>
-                <div class="m-0" v-html="post.content"></div>
+                <div class="m-0 px-2 py-2 " v-html="post.content"></div>
             </template>
 
             <template #footer>
+                <p class="text-sm text-gray-600 dark:text-gray-400 mt-5">Perbaharui pada : {{ formatDate(post.updated)
+                    }}</p>
                 <div class="flex justify-between mt-5">
                     <p>Created by : {{ post.created }}</p>
 
                     <div class="flex justify-end gap-2">
-                        <Button icon="pi pi-arrow-left" label="Kembali"
-                                severity="secondary"
-                                type="button"
-                                @click="router.get('/admin/posts/')"/>
+                        <Button icon="pi pi-arrow-left" label="Kembali" severity="secondary" type="button"
+                            @click="router.get('/admin/posts/')" />
 
-                        <Button icon="pi pi-pencil" label="Edit"
-                                type="button"
-                                @click="router.get(`/admin/posts/edit/${post.id}`)"/>
+                        <Button icon="pi pi-pencil" label="Edit" type="button"
+                            @click="router.get(`/admin/posts/edit/${post.id}`)" />
                     </div>
                 </div>
             </template>
