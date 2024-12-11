42d3af99c928119157fae77e32a4a02dcd40cb33
Mahendar <hendartea@gmail.com>
2024-12-11 10:53:29 +0700

    About:view visimisi dinamicly:

--- resources/js/Pages/Public/Home/Home.vue
+++ resources/js/Pages/Public/Home/Home.vue
@@ -104,7 +104,7 @@
 
                         <div class="flex items-center mt-5 -mx-2">
                             <img class="object-cover mx-2 rounded-full w-14 shrink-0 h-14 ring-4 ring-gray-300 dark:ring-gray-700"
-                                src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                                src="/images/dirdaa.png"
                                 alt="">
 
                             <div class="mx-2">
